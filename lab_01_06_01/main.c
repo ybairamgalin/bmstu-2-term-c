28c134bc3550320fbf0ec50367a1d1a49ddea9f7
Bairamgalin Yaroslav <1221-byar20k035@users.noreply.git.iu7.bmstu.ru>
2021-02-27 10:12:41 +0300

    first commit of task 6

---
+++ lab_01_06_01/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  lab01task05
+//
+//  Created by Yaroslav Bairamgalin on 27.02.2021.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    float x_a, y_a, x_b, y_b, x_c, y_c;
+    float det, square;
+    
+    int corr_input = scanf("%f%f%f%f%f%f", &x_a, &y_a, &x_b, &y_b, &x_c, &y_c);
+    if (corr_input < 6)
+        return 10;
+    
+    
+    // solve for signed square using det formula
+    det = (x_a - x_c) * (y_b - y_c) - (x_b - x_c) * (y_a - y_c);
+    square = det / 2;
+    
+    // using det formula determinant may and up being negative
+    square = square > 0 ? square: -square;
+    
+    printf("%f", square);
+    return 0;
+}
