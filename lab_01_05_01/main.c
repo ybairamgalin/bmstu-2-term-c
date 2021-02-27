433db8f52c9b33e5a7feb0fc9643b1409335a1ca
Bairamgalin Yaroslav <1221-byar20k035@users.noreply.git.iu7.bmstu.ru>
2021-02-27 13:04:59 +0300

    fixed exception

--- lab_01_05_01/main.c
+++ lab_01_05_01/main.c
@@ -11,13 +11,13 @@ int main()
 {
     int num, denom;
     int corr_input = scanf("%d%d", &num, &denom);
-    if (corr_input < 2 || corr_input == EOF)
+    if (corr_input != 2)
         return 10;
     if (num < 0 || denom <= 0)
         return 10;
     
     int quo = 0;
-    // num here finally equals rest
+    // num finally equals rest
     while (num >= denom)
     {
         quo++;
