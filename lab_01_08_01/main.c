8b91ddf0541c55c0c895241daf6a542e2bf641ae
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-03-02 15:12:10 +0300

    fixed style task 8

--- lab_01_08_01/main.c
+++ lab_01_08_01/main.c
@@ -8,10 +8,7 @@
 
 // function recieves 4 bytes and returns 4 bytes packed into a single var
 // all vars type is unsigned long int
-unsigned long int pack(unsigned long int byte_1,
-                       unsigned long int byte_2,
-                       unsigned long int byte_3,
-                       unsigned long int byte_4)
+unsigned long int pack(unsigned long int byte_1, unsigned long int byte_2, unsigned long int byte_3, unsigned long int byte_4)
 {
     unsigned long int res = byte_1 | byte_2 | byte_3 | byte_4;
     return res;
