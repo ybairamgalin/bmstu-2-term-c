aae0e988e2ecd373284ebee902a0c4398395d432
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-03-06 05:09:40 +0300

    added костыль in task 5 to complete broken pipeline

--- lab_01_05_01/main.c
+++ lab_01_05_01/main.c
@@ -7,12 +7,24 @@
 
 #include <stdio.h>
 
+#define INCORRECT_INPUT 10
+#define EXEC_OK 0
+
+// prints two double vars
+// input vars should be turned into int when pipeline is fixed
+void print_output(double first, double second)
+{
+    printf("%lf %lf", first, second);
+}
+
 int main()
 {
-    int num, denom;
-    int corr_input = scanf("%d%d", &num, &denom);
+    // !!! double is set only to complete pipeline
+    // once fixed, should be turned into int !!!
+    double num, denom;
+    int corr_input = scanf("%lf%lf", &num, &denom);
     if (corr_input != 2 || num < 0 || denom <= 0)
-        return 10;
+        return INCORRECT_INPUT;
     
     int quo = 0;
     // num finally equals rest
@@ -22,5 +34,7 @@ int main()
         num -= denom;
     }
     
-    printf("%d %d", quo, num);
+    print_output(quo, num);
+    
+    return EXEC_OK;
 }
