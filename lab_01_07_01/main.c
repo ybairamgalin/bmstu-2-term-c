1d7492cc8cd3964e5fdec263d60d9c5d016113e8
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-03-06 04:57:59 +0300

    added function in task7

--- lab_01_07_01/main.c
+++ lab_01_07_01/main.c
@@ -8,16 +8,29 @@
 #include <stdio.h>
 #include <math.h>
 
+#define INCORRECT_INPUT 10
+#define EXEC_OK 0
+
+// function recives two parametrs: double expected -
+// the precise value, double measured - measured or
+// approximated value
+// function returns abs error of the given data
+double abs_error(double expected, double measured)
+{
+    double result = fabs(expected - measured);
+    return result;
+}
+
 int main()
 {
     double x;
     double eps;
     int corr_input = scanf("%lf%lf", &x, &eps);
     if (corr_input < 2)
-        return 10;
+        return INCORRECT_INPUT;
     // due to the task
     if (eps <= 0 || eps > 1)
-        return 10;
+        return INCORRECT_INPUT;
     
     double precise_value = sin(x);
     double approx_value = x;
@@ -32,7 +45,7 @@ int main()
     }
     
     // errors
-    double absolute_error = fabs(precise_value - approx_value);
+    double absolute_error = abs_error(precise_value, approx_value);
     double relative_error = absolute_error / precise_value;
     
     printf("%lf %lf %lf %lf", approx_value, precise_value, absolute_error, relative_error);
