30c786257650b7648c03932946066ead16747c7b
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-03-09 13:18:45 +0300

    added functions task 1

--- lab_02_01_01/main.c
+++ lab_02_01_01/main.c
@@ -7,6 +7,30 @@
 #define YES 1
 #define NO 0
 
+// function finds the product of all negative elements and puts in into *product
+// if no such elements exist returns 0
+// else returns 1
+int solve(int *array, int arr_lng, int *product)
+{
+    int contains_odd = NO;
+    for (int i = 0; i < arr_lng; i++)
+    {
+        // != to avoid problems with negative elements
+        if (*(array + i) % 2 != 0)
+        {
+            *product *= *(array + i);
+            contains_odd = YES;
+        }
+    }
+    return contains_odd;
+}
+
+// function printa int number
+void print_output(int number)
+{
+    printf("%d", number);
+}
+
 int main()
 {
     int arr[ARRAY_SIZE];
@@ -15,7 +39,6 @@ int main()
     int corr_input = scanf("%d", &number_of_elements);
     if (corr_input != 1 || number_of_elements < 0 || number_of_elements > ARRAY_SIZE)
         return INCORRECT_INPUT;
-    int CONTAINS_ODD = NO;
     
     // prod - final product
     int prod = 1;
@@ -24,18 +47,11 @@ int main()
         corr_input = scanf("%d", arr + i);
         if (corr_input != 1)
             return INCORRECT_INPUT;
-        // != to avoid problems with negative elements
-        if (arr[i] % 2 != 0)
-        {
-            prod *= arr[i];
-            CONTAINS_ODD = YES;
-        }
     }
     
-    if (CONTAINS_ODD == NO)
+    if (solve(arr, number_of_elements, &prod) == 0)
         return INCORRECT_INPUT;
- 
-    printf("%d", prod);
-
+        
+    print_output(prod);
     return EXEC_OK;
 }
