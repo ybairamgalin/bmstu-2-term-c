2066fab4c9e6fc42f1b9900e17072577c3f7cb8e
Yaroslav <ybairamgalin@gmail.com>
2021-03-31 17:37:23 +0300

    task6: added tests, added procecc_3

--- lab_02_06/main.c
+++ lab_02_06/main.c
@@ -9,7 +9,7 @@
 #define YES 1
 #define NO 0
 
-#define ARRAY_SIZE 5000
+#define ARRAY_SIZE 10000
 #define MAX_NUMBER_OF_CYCLES 10000
 
 // function receives pointer to an array and number of elements in this array
@@ -61,7 +61,34 @@ int process_1(int arr[], int number_of_elements)
     return sum;
 }
 
-int process_2(int *arr, int number_of_elements)
+int process_2(int arr[], int number_of_elements)
+{
+    int sum = 0;
+    int i = 0, j = number_of_elements - 1;
+
+    while (i < number_of_elements && j >= 0)
+    {
+        if (*(arr + i) > 0)
+        {
+            i++;
+            continue;
+        }
+
+        if (*(arr + j) < 0)
+        {
+            j--;
+            continue;
+        }
+
+        sum += *(arr + i) * *(arr + j);
+        i++;
+        j--;
+    }
+
+    return sum;
+}
+
+int process_3(int *arr, int number_of_elements)
 {
     int sum = 0;
     int i = 0, j = number_of_elements - 1;
@@ -108,9 +135,8 @@ int main(void) {
         return INCORRECT_TYPE;
 
     struct timeval tv_start, tv_stop;
-    // elapsed_time_1 - время выполнения функции process_1,
-    // elapsed_time_2 - время выполнения функции process_2,
-    int64_t elapsed_time_1 = 0, elapsed_time_2 = 0;
+    // elapsed_time_N - время выполнения функции process_N,
+    int64_t elapsed_time_1 = 0, elapsed_time_2 = 0, elapsed_time_3 = 0;
 
     // measure process_1
     gettimeofday(&tv_start, NULL);
@@ -132,7 +158,19 @@ int main(void) {
         elapsed_time_2 += (tv_stop.tv_sec - tv_start.tv_sec) * 1000000LL + (tv_stop.tv_usec - tv_start.tv_usec);
     }
 
+    // measure process_2
+    for (int i = 0; i < number_of_cycles; i++)
+    {
+        gettimeofday(&tv_start, NULL);
+        process_3(input_array, number_of_elements);
+        gettimeofday(&tv_stop, NULL);
+        elapsed_time_3 += (tv_stop.tv_sec - tv_start.tv_sec) * 1000000LL + (tv_stop.tv_usec - tv_start.tv_usec);
+    }
+
+
+
     printf("%" PRId64 "\tmicroseconds (sec * (1e-6))\n", elapsed_time_1);
     printf("%" PRId64 "\tmicroseconds (sec * (1e-6))\n", elapsed_time_2);
+    printf("%" PRId64 "\tmicroseconds (sec * (1e-6))\n", elapsed_time_3);
 
 }
