6bc2214b5e8aec1efba855de14ecbc114dd0ad40
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-04-23 16:59:54 +0300

    ADD: task 3

---
+++ lab_04_03_01/functions.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include "functions.h"
+
+void print_result(char (*dest_array)[MAX_WORD_SIZE], const int size)
+{
+    printf("Result:");
+
+    for (int i = size - 1; i >= 0; i--)
+        if (strcmp(dest_array[i], EMPTY_STRING) != WORDS_ARE_EQUAL)
+            printf(" %s", dest_array[i]);
+}
+
+int modify_word(char *word, const int src, const int dest)
+{
+    int was_modified = NO;
+
+    if (word[src] == word[dest])
+    {
+        was_modified = YES;
+        for (int i = dest; word[i] != CHAR_END_OF_STR; i++)
+            word[i] = word[i + 1];
+    }
+
+    return was_modified;
+}
+
+void del_first_appear_letter(char *word)
+{
+    for (int i = 0; word[i] != CHAR_END_OF_STR; i++)
+        for (int j = i + 1; word[j] != CHAR_END_OF_STR; j++)
+            if (modify_word(word, i, j) == YES)
+                j--;
+}
+
+void set_as_last_empty(char (*array)[MAX_WORD_SIZE], const int size)
+{
+    for (int i = size - 2; i >= 0; i--)
+        if (strcmp(array[i], array[size - 1]) == WORDS_ARE_EQUAL)
+            strncpy(array[i], EMPTY_STRING, MAX_WORD_SIZE);
+}
+
+
+int split_and_solve(char *string, char (*dest_array)[MAX_WORD_SIZE])
+{
+    char *word = strtok(string, SEPARATORS);
+    int size = 0;
+
+    while (word != NULL)
+    {
+        strncpy(dest_array[size], word, sizeof(dest_array[size]) - 1);
+        word = strtok(NULL, SEPARATORS);
+        size++;
+    }
+
+    return size;
+}
---
+++ lab_04_03_01/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "functions.h"
+
+int main(void)
+{
+    setbuf(stdout, NULL);
+
+    char str[MAX_STR_SIZE];
+
+    if (fgets(str, MAX_STR_SIZE, stdin) == NULL)
+        return INPUT_ERROR;
+
+    if (str[strlen(str) - 1] != CHAR_END_OF_LINE)
+        return INPUT_ERROR;
+
+    char result_arr[MAX_WORDS][MAX_WORD_SIZE];
+    int arr_size = split_and_solve(str, result_arr);
+    set_as_last_empty(result_arr, arr_size);
+
+    for (int i = 0; i < arr_size; i++)
+        del_first_appear_letter(result_arr[i]);
+
+    print_result(result_arr, arr_size);
+
+    return OK;
+}
