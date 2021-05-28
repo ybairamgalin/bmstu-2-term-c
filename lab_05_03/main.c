b6453adb6e8318cbaecc7356ce3e5ec9d6ebdb63
Yaroslav <ybairamgalin@gmail.com>
2021-05-28 21:38:43 +0300

    UPD: all three parts are now functional

--- lab_05_03/main.c
+++ lab_05_03/main.c
@@ -8,13 +8,15 @@
 #define NOT_ENOUGH_COMMAND_LINE_ARGS -1
 #define COULD_NOT_CREATE_FILE -2
 #define FILE_DOES_NOT_EXIST -3
+#define POS_ERROR -4
+#define UNKNOWN_COMMAND -5
 
 #define EQUAL_STR 0
 
 #define NUMBER_OF_RANDOM_INT_NUMS 20
 
 
-int create_file(char *file_name)
+int create_file(const char *file_name)
 {
     char full_file_name[64];
     strcpy(full_file_name, "files/");
@@ -39,7 +41,7 @@ int create_file(char *file_name)
     return OK;
 }
 
-int read_int_from_binary(char *file_name)
+int print_int_from_binary(const char *file_name)
 {
     FILE *file;
     file = fopen(file_name, "rb");
@@ -64,6 +66,67 @@ int read_int_from_binary(char *file_name)
     return OK;
 }
 
+int get_number_by_pos(FILE *file, const int pos, int *num)
+{
+    fseek(file, pos * sizeof(int), SEEK_SET);
+    size_t read = fread(num, sizeof(int), 1, file);
+
+    if (read == 1)
+        return OK;
+    else
+        return POS_ERROR;
+}
+
+int put_number_by_pos(FILE *file, const int pos, const int num)
+{
+    fseek(file, pos * sizeof(int), SEEK_SET);
+    size_t put = fwrite(&num, sizeof(int), 1, file);
+
+    if (put == 1)
+        return OK;
+    else
+        return POS_ERROR;
+}
+
+void compare_and_swap(FILE *file, const int first, const int second)
+{
+    int num_1, num_2;
+
+    get_number_by_pos(file, first, &num_1);
+    get_number_by_pos(file, second, &num_2);
+
+    if (num_1 > num_2)
+    {
+        // swap
+        put_number_by_pos(file, second, num_1);
+        put_number_by_pos(file, first, num_2);
+    }
+}
+
+void bubble_sort_binary_file(FILE *file)
+{
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    for (int i = 0; i < size / sizeof(int); i++)
+        for (int j = i + 1; j < size / sizeof(int); j++)
+            compare_and_swap(file, i, j);
+}
+
+int sort_numbers_in_file(const char *filename)
+{
+    FILE *file;
+    file = fopen(filename, "rb+");
+
+    if (file == NULL)
+        return FILE_DOES_NOT_EXIST;
+
+    bubble_sort_binary_file(file);
+
+    return OK;
+}
+
 int main(const int argc, char **argv)
 {
     if (argc != 3)
@@ -76,13 +139,15 @@ int main(const int argc, char **argv)
     }
     else if (strcmp(argv[1], "p") == EQUAL_STR)
     {
-        if (read_int_from_binary(argv[2]) != OK)
+        if (print_int_from_binary(argv[2]) != OK)
             return FILE_DOES_NOT_EXIST;
     }
     else if (strcmp(argv[1], "s") == EQUAL_STR)
     {
-
+        sort_numbers_in_file(argv[2]);
     }
+    else
+        return UNKNOWN_COMMAND;
 
     return OK;
 }
