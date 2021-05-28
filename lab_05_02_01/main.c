154fbd8530f53721dfd48d9f4573b4bd3e9f6ba7
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-05-28 19:22:54 +0300

    UPD: added exception when file is empty

--- lab_05_02_01/main.c
+++ lab_05_02_01/main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
 #define NOT_ENOUGH_MAIN_ARGS -1
+#define NO_INTEGERS_IN_FILE -2
 
 #define MAX_INT 2147483647
+#define YES 1
+#define NO 0
 
-void find_max_min(FILE *file, int *max, int *min)
+int find_max_min(FILE *file, int *max, int *min)
 {
     int number;
     *max = -MAX_INT;
     *min = MAX_INT;
 
+    int number_exists = NO;
+
     while (fscanf(file, "%d", &number) != EOF)
     {
+        number_exists = YES;
+
         if (number > *max)
             *max = number;
 
         if (number < *min)
             *min = number;
     }
+
+    return number_exists;
 }
 
 int greater_than_average_nums(FILE *file, const int average)
@@ -42,7 +51,10 @@ int main(const int argc, char **argv)
     FILE *file = fopen(file_name, "r");
 
     int max, min;
-    find_max_min(file, &max, &min);
+
+    if (find_max_min(file, &max, &min) == NO)
+        return NO_INTEGERS_IN_FILE;
+
     int average = (max + min) / 2;
     fseek(file, 0, SEEK_SET);
     int result = greater_than_average_nums(file, average);
