5b89079172dafeca6adb41e28b842caf2e05115a
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-05-30 13:47:16 +0300

    UPD: try to fix memory error

--- lab_05_02_01/main.c
+++ lab_05_02_01/main.c
@@ -1,67 +1,37 @@
 #include <stdio.h>
-
-#define OK 0
-#define NOT_ENOUGH_MAIN_ARGS -1
-#define NO_INTEGERS_IN_FILE -2
-#define FILE_CANNOT_BE_OPENED -3
-
-#define MAX_INT 2147483647
-#define YES 1
-#define NO 0
-
-int find_max_min(FILE *file, int *max, int *min)
-{
-    int number;
-    *max = -MAX_INT;
-    *min = MAX_INT;
-
-    int number_exists = NO;
-
-    while (fscanf(file, "%d", &number) != EOF)
-    {
-        number_exists = YES;
-
-        if (number > *max)
-            *max = number;
-
-        if (number < *min)
-            *min = number;
-    }
-
-    return number_exists;
-}
-
-int greater_than_average_nums(FILE *file, const int average)
-{
-    int numbers_greater_than_average = 0;
-    int number;
-
-    while (fscanf(file, "%d", &number) != EOF)
-        if (number > average)
-            numbers_greater_than_average++;
-
-    return numbers_greater_than_average;
-}
+#include "functions.h"
 
 int main(const int argc, char **argv)
 {
     if (argc != 2)
+    {
+        printf("Error: not enough command line arguments\n");
+
         return NOT_ENOUGH_MAIN_ARGS;
+    }
 
     char *file_name = argv[1];
 
     FILE *file;
 
     if ((file = fopen(file_name, "r")) == NULL)
+    {
+        printf("Error: file does not exist\n");
+
         return FILE_CANNOT_BE_OPENED;
+    }
 
     int max, min;
 
     if (find_max_min(file, &max, &min) == NO)
-        return NO_INTEGERS_IN_FILE;
+    {
+        printf("Error: no integers in file\n");
 
-    int average = (max + min) / 2;
+        return NO_INTEGERS_IN_FILE;
+    }
+
     fseek(file, 0, SEEK_SET);
+    int average = (max + min) / 2;
     int result = greater_than_average_nums(file, average);
 
     printf("%d", result);
