226169336025cd39feac339679914918d543573a
Yaroslav <ybairamgalin@gmail.com>
2021-10-05 13:50:44 +0300

    UPD: filtering array is now happening out of main

--- lab_07_01_01/src/main.c
+++ lab_07_01_01/src/main.c
@@ -24,26 +24,15 @@ int main(const int argc, const char *argv[])
 
     int *result_start = arr, *result_end = arr + nums_in_file;
     read_nums_from_file(argv[1], arr, arr + nums_in_file);
-    int *new_arr_start = NULL, *new_arr_end = NULL;
 
     if (argc == MAX_COMMAND_LINE_ARGS && strcmp(argv[3], "f") == 0)
-    {
-        if ((error = key(arr, arr + nums_in_file, &new_arr_start, &new_arr_end))
-                != EXIT_SUCCESS)
-        {
-            free(arr);
+        if ((error = filter_del_src(arr, arr + nums_in_file,
+            &result_start, &result_end)) != EXIT_SUCCESS)
             return error;
-        }
-
-        result_start = new_arr_start;
-        result_end = new_arr_end;
-    }
 
     mysort(result_start, result_end - result_start, sizeof(int), int_cmp);
     put_int_arr_in_file(argv[2], result_start, result_end);
-
-    free(arr);
-    free(new_arr_start);
+    free(result_start);
 
     return EXIT_SUCCESS;
 }
