97a4fb06347ab79812c7c9e4171e91358332f845
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2021-11-06 16:24:10 +0300

    UPD: defined command line args count

--- lab_08_12_11/src/main.c
+++ lab_08_12_11/src/main.c
@@ -2,23 +2,27 @@
 #include <string.h>
 #include "matrix.h"
 
+#define ADDITION_CL_COUNT 5
+#define MULTIPLICATION_CL_COUNT 5
+#define DETERMINANT_CL_COUNT 4
+
 int main(const int argc, const char **argv)
 {
     int error;
 
-    if (argc == 5 && strcmp(argv[1], "a") == 0)
+    if (argc == ADDITION_CL_COUNT && strcmp(argv[1], "a") == 0)
     {
         if ((error = add_matrices_from_file(argv[2], argv[3], argv[4])) !=
         EXIT_SUCCESS)
             return error;
     }
-    else if (argc == 5 && strcmp(argv[1], "m") == 0)
+    else if (argc == MULTIPLICATION_CL_COUNT && strcmp(argv[1], "m") == 0)
     {
         if ((error = multiply_matrices_from_file(argv[2], argv[3], argv[4])) !=
         EXIT_SUCCESS)
             return error;
     }
-    else if (argc == 4 && strcmp(argv[1], "o") == 0)
+    else if (argc == DETERMINANT_CL_COUNT && strcmp(argv[1], "o") == 0)
     {
         if ((error = det_matrix_from_file(argv[2], argv[3])))
             return error;
